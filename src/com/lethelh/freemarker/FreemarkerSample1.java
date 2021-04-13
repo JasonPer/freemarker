eddf4f7cd5da13f9dce5b6846fa9063a1b564f31
jsonper <m15191678056@163.com>
2021-04-13 20:42:26 +0800

    freemarker 初体验.

---
+++ src/com/lethelh/freemarker/FreemarkerSample1.java
@@ -0,0 +1,32 @@
+package com.lethelh.freemarker;
+
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.util.HashMap;
+import java.util.Map;
+
+import freemarker.core.ParseException;
+import freemarker.template.Configuration;
+import freemarker.template.MalformedTemplateNameException;
+import freemarker.template.Template;
+import freemarker.template.TemplateException;
+import freemarker.template.TemplateNotFoundException;
+
+public class FreemarkerSample1 {
+	public static void main(String[] args) throws TemplateNotFoundException, MalformedTemplateNameException, ParseException, IOException, TemplateException{
+		//1. 加载模板
+		//创建核心配置对象
+		Configuration config = new Configuration(Configuration.VERSION_2_3_31);
+		//设置加载目录
+		config.setClassForTemplateLoading(FreemarkerSample1.class, "");
+		//得到模板对象
+		Template t = config.getTemplate("sample1.ftl");
+		//2. 创建数据
+		Map<String, Object> data = new HashMap<String,Object>();
+		data.put("site", "百度");
+		data.put("url", "http://www.baidu.com");
+		//3. 产生输出
+		t.process(data, new OutputStreamWriter(System.out));
+	}
+
+}
